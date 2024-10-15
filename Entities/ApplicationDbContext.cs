02ac67d3cda0212abb63c15cc0dbf432c81016ae
Badri <mahmoud.elbadry357@gmail.com>
2024-10-16 00:05:21 +0300

    Rest of the requirementsAdd project files.

--- Entities/ApplicationDbContext.cs
+++ Entities/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Entities.IdentityEntities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -9,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace Entities
 {
-    public sealed class ApplicationDbContext : IdentityDbContext<IdentityUser>, IDbContext
+    public sealed class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
     {
         public DbSet<Movie> Movies { get; set; }
         public DbSet<Review> Reviews { get; set; }
